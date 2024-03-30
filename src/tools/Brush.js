df912919e24682184231d48735eab5b15637cd86
Maxim Mineev <mms1401@ya.ru>
2024-03-30 16:27:11 +0500

    add Brush, add Rect

--- src/tools/Brush.js
+++ src/tools/Brush.js
@@ -13,14 +13,23 @@ export default class Brush extends Tool {
     }
 
     mouseUpHandler(e) {
-
+        this.mouseDown = false;
     }
 
     mouseDownHandler(e) {
-
+        this.mouseDown = true;
+        this.ctx.beginPath();
+        this.ctx.moveTo(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
     }
 
     mouseMoveHandler(e) {
+        if (this.mouseDown) {
+            this.draw(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
+        }
+    }
 
+    draw(x, y) {
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
     }
 }
