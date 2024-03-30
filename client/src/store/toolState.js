1a7a4b1173fd015045263bf125d2b47339c46718
Maxim Mineev <mms1401@ya.ru>
2024-03-30 23:57:28 +0500

    finish project

--- client/src/store/toolState.js
+++ client/src/store/toolState.js
@@ -1,26 +1,23 @@
-import { makeAutoObservable } from "mobx";
+import {makeAutoObservable} from "mobx";
 
 class ToolState {
-    tool = null;
+    tool = null
     constructor() {
-        makeAutoObservable(this);
+        makeAutoObservable(this)
     }
 
     setTool(tool) {
-        this.tool = tool;
+        this.tool = tool
     }
-
     setFillColor(color) {
-        this.tool.fillColor = color;
+        this.tool.fillColor = color
     }
-
     setStrokeColor(color) {
-        this.tool.strokeColor = color;
+        this.tool.strokeColor = color
     }
-
     setLineWidth(width) {
-        this.tool.lineWidth = width;
+        this.tool.lineWidth = width
     }
 }
 
-export default new ToolState();
+export default new ToolState()
